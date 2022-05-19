a87a054f97c6a716abe1d3031b235f56056df611
a.breezy <ambrose.wilkins@gmail.com>
2022-05-19 20:39:30 +0100

    created two practice forms and the User class

---
+++ tech-news-java-api/src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,15 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 1;
+        double b = 2.5;
+        double sum = a + b;
+
+        String c = "cat";
+        String oneCat = a + " " + c;
+
+        System.out.println(sum);
+        System.out.println(oneCat);
+    }
+}
